7823df70a2be0834e34466c6335939f6c93f3ca8
Bikashsah4m <87253056+Bikashsah4m@users.noreply.github.com>
2022-03-07 01:42:50 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
+    int  n;
     cin>>n;
-
-    for(int i = 0;i<n; i++){
-       int  k = i+1;
-        for(int j = 0; j<=i;j++){
-            cout<<k;
-            k = k+1;
-        }
-        cout<<endl;
-        }
+    for( int i =0;i<n;i++){
+        int l = i+1;
+    for(int j = 0;j<i:j++){
         
+        for(int k =0;k<n-j;k++){
+            cout<<" ";
+        }
+        for(int o =0;o <j;o++){
+            cout<<i;
+        }
+    }
+    cout<<endl;
+    }
 }
