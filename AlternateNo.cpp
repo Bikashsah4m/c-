b4ebe36bfc05274605d40bfc3d399a7b5e9e8bc6
Bikashsah4m <87253056+Bikashsah4m@users.noreply.github.com>
2022-03-12 08:38:16 +0530

    Add files via upload

---
+++ AlternateNo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+ int main(){
+     int n;
+    cin>>n;
+    int A[10000];
+    for(int i=0;i<n;i=i+1){
+     cin>>A[i];
+    }
+    for (int j=0;j<n;j=j+2){
+        cout<<A[j]<<endl;
+    }
+ }
